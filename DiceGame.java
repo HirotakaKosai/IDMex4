9988983c7f56de43ad332a9375062c6153162453
HirotakaKosai <s1260155@u-aizu.ac.jp>
2019-07-11 19:17:04 +0900

    Initial revision

---
+++ DiceGame.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class DiceGame {
+	public static void main(String[] args) {
+	Random r = new Random();
+	
+	int a = r.nextInt(6);
+	int b = r.nextInt(6);
+	int total=a+b;
+
+	System.out.println("Rolling the dice...");
+	System.out.println("Die 1:"+a);
+	System.out.println("Die 2:"+b);
+	System.out.println("Total Value:"+total);
+
+	}
+}
