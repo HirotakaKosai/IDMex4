8c0c207b30454e24cd17ccfb8635addc8da4969e
HirotakaKosai <s1260155@u-aizu.ac.jp>
2019-07-11 19:44:06 +0900

    New feature revision

--- DiceGame.java
+++ DiceGame.java
@@ -9,6 +9,7 @@ public class DiceGame {
 	int a = r.nextInt(6);
 	int b = r.nextInt(6);
 	int total=a+b;
+	int cnt=0;
 
 
 	System.out.println("What is your name ?");
@@ -27,5 +28,23 @@ public class DiceGame {
 	}else {
 		System.out.println(str + "You lose");
 	}
+	while(total%2 != 0) {
+		System.out.println("One more dice...");
+		a = r.nextInt(6);
+		b = r.nextInt(6);
+		total=a+b;
+		System.out.println("Die 1:"+a);
+		System.out.println("Die 2:"+b);
+		System.out.println("Total Value:"+total);
+		if(total>7) {
+			System.out.println( str + " won!");
+		}else {
+			System.out.println( str + " lose");
+		}
+		cnt++;
 	}
-}
+	System.out.println("Your odd number count is "+cnt);
+	System.out.println("Finished");
+	}
+	}
+
